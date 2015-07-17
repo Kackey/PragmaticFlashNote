39ae2744ebcc470dd374cd56fd7cf334fbcee2ad
Hidekazu Kakinuma <hidekazu.kakinuma@rakuten.com>
2015-07-17 10:13:51 +0900

    [#99265604]

--- JS/CodingKata/bowling_game/first_test/step-1_Red/bowling_game.spec.js
+++ JS/CodingKata/bowling_game/first_test/step-1_Red/bowling_game.spec.js
@@ -1,12 +1,7 @@
-/**
- * Created by hide on 15/04/14.
- */
-
 describe ('Bowling', function(){
     describe ('Gutter Game', function(){
         it ('should have game object.', function(){
             var game = BowlingGame.create();
-        })
-    })
-
-})
+        });
+    });
+});
